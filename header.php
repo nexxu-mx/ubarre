f5492725d334e97eeaab9621945a3745881a6eef
EduardoCervantes2706 <luis_edu_cj@hotmail.com>
2025-08-28 13:09:06 -0600

    logo header corregido

--- header.php
+++ header.php
@@ -1,18 +1,17 @@
-
 <header class="header header-desktop" data-header>
   <div class="adorno-navbar"></div>
-<div class="container">
+  <div class="container">
 
-  <a href="index.php" class="logo">
-    <img src="./assets/images/ubarre/svg/ubarre-negro.svg" width="136" height="46"
-      alt>
-  </a>
-  <nav class="navbar">
+    <a href="index.php" class="logo">
+      <img src="./assets/images/ubarre/SVG/ubarre-negro.svg" width="136" height="46"
+        alt>
+    </a>
+    <nav class="navbar">
 
       <div class="navbar-top">
 
         <a href="index.php" class="logo">
-          <img src="./assets/images/svg/logo-negro.svg" width="136"
+          <img src="./assets/images/ubarre/SVG/ubarre-negro.svg" width="136"
             height="46" alt>
         </a>
 
@@ -81,15 +80,15 @@
 
       </ul>
 
-      </nav>
-  
+    </nav>
 
-  <div class="container-profile-menu">
-    <?php
-    
-    session_start();
-    if (empty($_SESSION['idUser']) || empty($_SESSION['nombre'])) {
-        
+
+    <div class="container-profile-menu">
+      <?php
+
+      session_start();
+      if (empty($_SESSION['idUser']) || empty($_SESSION['nombre'])) {
+
         echo '<div class="login-bolsa-navbar">
             <a href="login.php" style="display: flex;align-items: baseline;gap: 3px;">
               <img src="assets/images/svg/login.svg" alt="login icon">
@@ -103,9 +102,8 @@
             <p style="font-family: sans-serif;"> 0 </p>
           </div> 
           ';
-        
-    }else{
-      echo '<div class="login-bolsa-navbar">
+      } else {
+        echo '<div class="login-bolsa-navbar">
             <a href="profile.php" style="display: flex;align-items: baseline;gap: 3px;font-weight: 400;">
               <img src="assets/images/svg/login.svg" alt="login icon">
               <p id="profile-name">' . $_SESSION['nombre'] . '</p>
@@ -118,98 +116,98 @@
             </a>
             <p style="font-family: sans-serif;> 0 </p>
           </div> ';
-    }
-    
- 
-    
-    ?>
+      }
 
-    <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
-      <ion-icon name="menu-outline"></ion-icon>
-    </button>
+
+
+      ?>
+
+      <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+        <ion-icon name="menu-outline"></ion-icon>
+      </button>
+    </div>
+
+
+
   </div>
-
-
- 
-</div>
 </header>
 
-  
+
 
 <div class="header-mobile" id="header-mobile">
-<a href="index.php" class="hmobile"><ion-icon name="home" aria-hidden="true"></ion-icon></a>
-<a href="reserva.php" class="hmobile"><ion-icon name="calendar-number-outline" aria-hidden="true"></ion-icon></a>
-<a href="login.php" class="hmobile">
+  <a href="index.php" class="hmobile"><ion-icon name="home" aria-hidden="true"></ion-icon></a>
+  <a href="reserva.php" class="hmobile"><ion-icon name="calendar-number-outline" aria-hidden="true"></ion-icon></a>
+  <a href="login.php" class="hmobile">
     <img src="assets/images/svg/login.svg" style="width: 27px;" alt="login icon">
-</a>
-<button class="nav-open-btn mobile-log" aria-label="open menu" data-nav-toggler="">
+  </a>
+  <button class="nav-open-btn mobile-log" aria-label="open menu" data-nav-toggler="">
     <ion-icon name="menu-outline" style="color: #2e2e2e;"></ion-icon>
-</button>
+  </button>
 
 </div>
 <nav class="navbar navbar-mob" data-navbar>
 
-      <div class="navbar-top">
+  <div class="navbar-top">
 
-        <a href="index.php" class="logo">
-          <img src="./assets/images/svg/logo-negro.svg" width="136"
-            height="46" alt>
-        </a>
+    <a href="index.php" class="logo">
+      <img src="./assets/images/svg/logo-negro.svg" width="136"
+        height="46" alt>
+    </a>
 
-        <button class="nav-close-btn" aria-label="clsoe menu"
-          data-nav-toggler>
-          <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
-        </button>
+    <button class="nav-close-btn" aria-label="clsoe menu"
+      data-nav-toggler>
+      <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+    </button>
 
-      </div>
+  </div>
 
-      <ul class="navbar-list">
+  <ul class="navbar-list">
 
-        <li class="navbar-item">
-          <a href="aboutus.php" class="navbar-link title-md">ABOUT US</a>
-        </li>
+    <li class="navbar-item">
+      <a href="aboutus.php" class="navbar-link title-md">ABOUT US</a>
+    </li>
 
-        <li class="navbar-item">
-          <a href="clases.php" class="navbar-link title-md">CLASES</a>
-        </li>
+    <li class="navbar-item">
+      <a href="clases.php" class="navbar-link title-md">CLASES</a>
+    </li>
 
-        <li class="navbar-item">
-          <a href="reserva.php" class="navbar-link title-md">RESERVA</a>
-        </li>
+    <li class="navbar-item">
+      <a href="reserva.php" class="navbar-link title-md">RESERVA</a>
+    </li>
 
-        <li class="navbar-item">
-          <a href="paquetes.php" class="navbar-link title-md">PAQUETES</a>
-        </li>
+    <li class="navbar-item">
+      <a href="paquetes.php" class="navbar-link title-md">PAQUETES</a>
+    </li>
 
-        <li class="navbar-item">
-          <a href="coaches.php" class="navbar-link title-md">COACHES</a>
-        </li>
+    <li class="navbar-item">
+      <a href="coaches.php" class="navbar-link title-md">COACHES</a>
+    </li>
 
-        <li class="navbar-item">
-          <a href="contacto.php" class="navbar-link title-md">CONTACTO</a>
-        </li>
+    <li class="navbar-item">
+      <a href="contacto.php" class="navbar-link title-md">CONTACTO</a>
+    </li>
 
-      </ul>
+  </ul>
 
-      <ul class="social-list">
-
-
-        <li>
-          <a href="#" class="social-link">
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a>
-        </li>
-
-        
-
-        <li>
-          <a href="#" class="social-link">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </a>
-        </li>
+  <ul class="social-list">
 
 
-      </ul>
+    <li>
+      <a href="#" class="social-link">
+        <ion-icon name="logo-facebook"></ion-icon>
+      </a>
+    </li>
 
-      </nav>
-      <div class="overlay" data-nav-toggler data-overlay></div>
+
+
+    <li>
+      <a href="#" class="social-link">
+        <ion-icon name="logo-instagram"></ion-icon>
+      </a>
+    </li>
+
+
+  </ul>
+
+</nav>
+<div class="overlay" data-nav-toggler data-overlay></div>
