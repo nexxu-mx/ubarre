3043408a4aacfcfa14f8c5b137a4591ce252fe77
EduardoCervantes2706 <luis_edu_cj@hotmail.com>
2025-09-10 14:50:47 -0600

    cambios en el icono de smoothies en app ssidebar

--- app/sidebar.php
+++ app/sidebar.php
@@ -1,110 +1,125 @@
+<style>
+	.ionicon {
+		width: 1.3rem;
+		margin-left: .2rem;
+		fill: #8d9498;
+	}
+
+	a:hover>.ionicon {
+		fill: #0d6efd;
+		color: #0d6efd;
+	}
+</style>
+
 <div class="sidebar" data-background-color="">
-			<div class="sidebar-logo">
-				<!-- Logo Header -->
-				<div class="logo-header" data-background-color="">
+	<div class="sidebar-logo">
+		<!-- Logo Header -->
+		<div class="logo-header" data-background-color="">
 
-					<a href="index.php" class="logo">
-                        <img src="../assets/images/svg/logo-negro.svg"  height="186"
-                        alt>
-                    </a>
-					<div class="nav-toggle">
-						<button class="btn btn-toggle toggle-sidebar">
-							<i class="gg-menu-right"></i>
-						</button>
-						<button class="btn btn-toggle sidenav-toggler">
-							<i class="gg-menu-left"></i>
-						</button>
-					</div>
-					<button class="topbar-toggler more">
-						<i class="gg-more-vertical-alt"></i>
-					</button>
-
-				</div>
-				<!-- End Logo Header -->	
-			</div>	
-			<div class="sidebar-wrapper scrollbar scrollbar-inner">
-				<div class="sidebar-content">
-					<ul class="nav nav-secondary">
-						<li class="nav-item">
-							<a href="index.php">
-							  <i class="fas fa-home"></i>
-							  <p>Inicio</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="registra-class.php">
-							  <i class="fas fa-qrcode"></i>
-							  <p>Asistencia</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="clientes.php">
-							  <i class="fas fa-users"></i>
-							  <p>Clientes</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="clases.php">
-							  <i class="fas fa-award"></i>
-							  <p>Clases</p>
-							</a>
-						</li>
-						
-						<li class="nav-item">
-							<a href="finanzas.php">
-							  <i class="fas fa-chart-bar"></i>
-							  <p>Finanzas</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="coach.php">
-							  <i class="fas fa-user-circle"></i>
-							  <p>Coaches</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="diciplinas.php">
-							  <i class="fas fa-graduation-cap"></i>
-							  <p>Disciplinas</p>
-							</a>
-						</li>
-						
-							<li class="nav-item">
-							<a href="users.php">
-							  <i class="fas fa-user-cog"></i>
-							  <p>Usuarios</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="paquetes.php">
-							  <i class="fas fa-boxes"></i>
-							  <p>Paquetes</p>
-							</a>
-						</li>
-
-						<li class="nav-item">
-							<a href="smoothies.php">
-							  <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-								<title>Pint</title><path d="M399 99.29L394 16H118.45L113 99.26c-1.29 19.24-2.23 33.14 3.73 65.66 1.67 9.11 5.22 
-								22.66 9.73 39.82 12.61 48 33.71 128.36 33.71 195.63V496h191.68v-95.62c0-77.09 21.31-153.29 34-198.81 4.38-15.63 7.83-28 
-								9.41-36.62 6.01-32.51 5.07-46.42 3.74-65.66zM146.23 80l2-32h215.52l2 32z"/></svg>
-							  <p>Smoothies</p>
-							</a>
-						</li>
-
-						<li class="nav-item">
-							<a href="../profile.php">
-							  <i class="fas fa-power-off"></i>
-							  <p>Salir</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="soporte.php">
-							  <i class="fas fa-headset"></i>
-							  <p>Soporte</p>
-							</a>
-						</li>
-					</ul>
-				</div>
+			<a href="index.php" class="logo">
+				<img src="../assets/images/svg/logo-negro.svg" height="186"
+					alt>
+			</a>
+			<div class="nav-toggle">
+				<button class="btn btn-toggle toggle-sidebar">
+					<i class="gg-menu-right"></i>
+				</button>
+				<button class="btn btn-toggle sidenav-toggler">
+					<i class="gg-menu-left"></i>
+				</button>
 			</div>
+			<button class="topbar-toggler more">
+				<i class="gg-more-vertical-alt"></i>
+			</button>
+
+		</div>
+		<!-- End Logo Header -->
+	</div>
+	<div class="sidebar-wrapper scrollbar scrollbar-inner">
+		<div class="sidebar-content">
+			<ul class="nav nav-secondary">
+				<li class="nav-item">
+					<a href="index.php">
+						<i class="fas fa-home"></i>
+						<p>Inicio</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="registra-class.php">
+						<i class="fas fa-qrcode"></i>
+						<p>Asistencia</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="clientes.php">
+						<i class="fas fa-users"></i>
+						<p>Clientes</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="clases.php">
+						<i class="fas fa-award"></i>
+						<p>Clases</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="finanzas.php">
+						<i class="fas fa-chart-bar"></i>
+						<p>Finanzas</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="coach.php">
+						<i class="fas fa-user-circle"></i>
+						<p>Coaches</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="diciplinas.php">
+						<i class="fas fa-graduation-cap"></i>
+						<p>Disciplinas</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="users.php">
+						<i class="fas fa-user-cog"></i>
+						<p>Usuarios</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="paquetes.php">
+						<i class="fas fa-boxes"></i>
+						<p>Paquetes</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="smoothies.php" style="gap: 1.3rem;">
+						<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
+							<title>Pint</title>
+							<path d="M399 99.29L394 16H118.45L113 99.26c-1.29 19.24-2.23 33.14 3.73 65.66 1.67 9.11 5.22 
+								22.66 9.73 39.82 12.61 48 33.71 128.36 33.71 195.63V496h191.68v-95.62c0-77.09 21.31-153.29 34-198.81 4.38-15.63 7.83-28 
+								9.41-36.62 6.01-32.51 5.07-46.42 3.74-65.66zM146.23 80l2-32h215.52l2 32z" />
+						</svg>
+						<p>Smoothies</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="../profile.php">
+						<i class="fas fa-power-off"></i>
+						<p>Salir</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="soporte.php">
+						<i class="fas fa-headset"></i>
+						<p>Soporte</p>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </div>
