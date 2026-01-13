31cedbc16b463c7125d06dcd45f76e1d079c3dd5
Eduardo Cervantes Jaramillo <luis_edu_cj@hotmail.com>
2026-01-13 14:58:11 -0600

    credenciales cambiadas en db para produccion

--- db.php
+++ db.php
@@ -2,14 +2,18 @@
 $devON = false;
 if ($devON == false) {
     $servername = "127.0.0.1";
-    $username = "u379047759_ubarrelem"; 
+    $username = "u379047759_ubarrelem";
     $password = "Ubarre25?";
     $database = "u379047759_ubarretea";
 } else {
-    $servername = "82.197.82.15";
+    /* $servername = "82.197.82.15";
     $username = "u379047759_ubarreN";
     $password = "Ubarre2025*";
-    $database = "u379047759_ubarre";
+    $database = "u379047759_ubarre"; */
+    $servername = "db";
+    $username = "root";
+    $password = "root";
+    $database = "ubarre";
 }
 
 $conn = new mysqli($servername, $username, $password, $database);
