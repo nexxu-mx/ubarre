0d2660a988df7dc596c443d56dd858f176faf11e
JuanFrancisco <juanfranjfob@gmail.com>
2026-06-02 12:04:30 -0600

    validacion de smothies en paquetes

--- app/procesar-paquete.php
+++ app/procesar-paquete.php
@@ -10,7 +10,7 @@ if (
     isset($_POST['invitados_paquete']) &&
     isset($_POST['personas_paquete']) &&
     isset($_POST['smoothies_paquete'])
-   
+
 ) {
     $nombrePaquete = trim(strtoupper($_POST['nombre_paquete']));
     $numeroClases = trim($_POST['numero_clases']);
@@ -23,26 +23,28 @@ if (
 
 
     $smoothieCheck = isset($_POST['smoothie_check']) ? 1 : 0;
-    $smoothiesPaquete = trim($_POST['smoothies_paquete']);
-    $ilimitcheck = isset($_POST['ilimit_check']) ? 1: 0;
+    //$smoothiesPaquete = trim($_POST['smoothies_paquete']);
+    $smoothiesPaquete = $smoothieCheck ? (int)trim($_POST['smoothies_paquete']) : "";
+
+    $ilimitcheck = isset($_POST['ilimit_check']) ? 1 : 0;
 
     $descuento = trim($_POST['dsc']);
     $descuento = $descuento === '' ? null : $descuento;
-    if(empty($descuento)){
+    if (empty($descuento)) {
         $finalizadsc = null;
-    }else{
-    $finalizadsc = trim($_POST['vigen']);
-    $finalizadsc = $finalizadsc === '' ? null : $finalizadsc;
+    } else {
+        $finalizadsc = trim($_POST['vigen']);
+        $finalizadsc = $finalizadsc === '' ? null : $finalizadsc;
     }
 
     if (isset($_POST['id_paquete_edit'])) {
         $idPaqueteEdit = $_POST['id_paquete_edit'];
 
         $updatePaquete = $conn->prepare("UPDATE paquetes SET clases = ?, costo = ?, nombre = ?, vigencia = ?, invitados = ?, persona = ?, descuento = ?, finalizadsc = ?, smoothies = ?, total_smoothies = ?, ilimitado = ? WHERE id = ?");
-        $updatePaquete->bind_param("sssssssssisi", $numeroClases, $costoPaquete, $nombrePaquete, $vigenciaPaquete, $invitadosPaquete, $personasPaquete, $descuento, $finalizadsc, $smoothieCheck, $smoothiesPaquete, $ilimitcheck, $idPaqueteEdit); 
+        $updatePaquete->bind_param("sssssssssssi", $numeroClases, $costoPaquete, $nombrePaquete, $vigenciaPaquete, $invitadosPaquete, $personasPaquete, $descuento, $finalizadsc, $smoothieCheck, $smoothiesPaquete, $ilimitcheck, $idPaqueteEdit);
 
         $resultadoUpdatePaquete = $updatePaquete->execute();
-        
+
         header('location: paquetes.php');
         exit;
     } else {
